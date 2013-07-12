e6093dea0bc2c0b6bace419ee0e55c634c06665b
robinsharma <sharms115@gmail.com>
2013-07-12 04:09:04 +0000

    File read fix2

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var fs = require('fs');
 
-var buffer = fs.readFileSync('/index.html', "utf-8");
+var buffer = fs.readFileSync('index.html', "utf-8");
 
 var app = express.createServer(express.logger());
 
